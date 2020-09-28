8141851789d1a760dae73a9fd4d67120e8214677
Gabriel Esnack <gabriel_esnack@hotmail.com>
2020-09-28 00:16:09 -0300

    update added chakra-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Heading, Flex } from '@chakra-ui/core';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi buddie!</h1>
+        <Flex align="center" w="100vw" justify="center">
+          <Heading as="h1" color="blue">
+            Wellcome The First Page. I am using the Chakra-UI
+          </Heading>
+        </Flex>
       </div>
     );
   }
