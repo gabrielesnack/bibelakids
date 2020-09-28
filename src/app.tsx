1afd3e0e8506b9082ccc210b2d8d77d38dc42adf
Gabriel Esnack <gabriel_esnack@hotmail.com>
2020-09-28 01:22:21 -0300

    fix added theme provider and reset css to clear style

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './router';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ThemeProvider>
+      <CSSReset>
+        <Routes />
+      </CSSReset>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
